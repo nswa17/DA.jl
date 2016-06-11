b2aec4f1573e09d9344d86e1324a386fb15fbd86
nswa17 <nswa17@gmail.com>
2016-06-11 16:22:59 +0900

    guess it works

--- da.jl
+++ da.jl
@@ -1,19 +1,11 @@
 #DA algorithm
 #todo: @time to compare recursive version and normal version
 #    : test function
-#    : make code readable
 #    : order
-#    : not
 #    : what if m_pointers[i] exceeds n
 #    : change arrange_m_offers into ~
-#    : error
-#    : bug if call_match(3, 3, Int[1 2 3; 2 1 3; 2 3 1], Int[1 2 3; 2 1 3; 1 2 3])
 #    : check function
-
 #recursive version
-#
-#input: m, n, 1d array of m_prefs, 1d array of f_prefs
-#output: m-elements 1d array, n-elements 1d array
 #
 
 function call_match(m::Int, n::Int, m_prefs, f_prefs)
@@ -30,14 +22,17 @@ function call_match(m::Int, n::Int, m_prefs, f_prefs)
 end
 
 function convert_pointer_to_list(m, m_pointers, f_pointers, f_prefs)
-    f_m = [f_prefs[j, f_pointer] for (j, f_pointer) in enumerate(f_pointers)]
+    f_m = [f_prefs[f_pointer, j] for (j, f_pointer) in enumerate(f_pointers)]
     m_f = [findfirst(f_m, i) for i in 1:m]
     return m_f, f_m
 end
 
-function proceed_pointer!(m, m_pointers, m_matched)
+function proceed_pointer!(m, n, m_pointers, m_matched, m_prefs)
     for i in 1:m
-        if !m_matched[i]
+        if m_prefs[m_pointers[i] + 1, i] == 0
+            m_matched[i] = true
+            m_pointers[i] += 1
+        elseif !m_matched[i]
             m_pointers[i] += 1
         end
     end
@@ -66,34 +61,25 @@ end
 
 
 function decide_to_accept!(m, n, f_pointers, f_prefs, m_offers, m_matched)######a bug is here
-    #arranged_offers = arrange_offers(m, n, m_offers)
-    #println(arranged_offers) #m_offers[i] is the male i's favorite female
     best_male_pointers = get_best_male_pointers(m, n, m_offers, f_prefs)
     for j in 1:n
         if f_pointers[j] > best_male_pointers[j] && best_male_pointers[j] != 0
             m_matched[f_prefs[best_male_pointers[j], j]] = true
-            m_matched[f_prefs[f_pointers[j], j]] = false
-            println("m_matched")
-            println(m_matched)
-            f_pointers[j] = best_male_pointers[j]#findfirst returns zero if it couldn't find the 2nd argument in the 1st argument.#could be an error
-        elseif f_pointers[j] == 0 && best_male_pointers[j] != 0
-            m_matched[f_prefs[best_male_pointers[j], j]] = true
-            f_pointers[j] = best_male_pointers[j]
+            if f_prefs[f_pointers[j], j] != 0
+                m_matched[f_prefs[f_pointers[j], j]] = false
+            end
+            f_pointers[j] = best_male_pointers[j]#findfirst returns zero if it couldn't find the 2nd argument in the 1st argument.
         end
-        println(f_pointers)
     end
 end
 
 function da_match(m, n, m_prefs, f_prefs, m_pointers, f_pointers, m_matched, m_offers)#m offers to f
-    proceed_pointer!(m, m_pointers, m_matched)
-    println("called:0")
+    proceed_pointer!(m, n, m_pointers, m_matched, m_prefs)
+    #println("called:0")
     create_offers!(m, m_prefs, m_matched, m_pointers, m_offers)
-    println("called:1")
-
+    #println("called:1")
     decide_to_accept!(m, n, f_pointers, f_prefs, m_offers, m_matched)
-    println("called:2")
-
-    println(m_matched)
+    #println("called:2")
     if all(m_matched) == true
         return f_pointers
     else
@@ -101,4 +87,5 @@ function da_match(m, n, m_prefs, f_prefs, m_pointers, f_pointers, m_matched, m_o
     end
 end
 
-println(call_match(3, 3, Int[1 2 1; 2 1 3; 3 3 2], Int[1 2 1; 2 1 2; 3 3 3]))
+println(call_match(3, 3, Int[1 0 1; 2 1 3; 3 3 2; 0 2 0], Int[1 2 0; 2 1 2; 3 3 3; 0 0 1]))
+println(call_match(3, 3, Int[1 2 3; 2 1 3; 2 3 1; 0 0 0], Int[1 2 3; 2 1 3; 1 2 3; 0 0 0]))
