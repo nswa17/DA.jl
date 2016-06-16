65b1f395fa5d782eb9a77412f777dd129decda5f
nswa17 <nswa17@gmail.com>
2016-06-16 19:01:31 +0900

    Slightly modify proceed_ptr

--- da.jl
+++ da.jl
@@ -56,13 +56,11 @@ end
 
 function proceed_pointer!(m::Int, n::Int, m_pointers, m_matched_tf, m_prefs)
     for i in 1:m
-        if m_pointers[i] + 1 > n + 1
-            m_matched_tf[i] = true
-        elseif m_prefs[m_pointers[i] + 1, i] == 0
-            m_matched_tf[i] = true
+        if !m_matched_tf[i]
             m_pointers[i] += 1
-        elseif !m_matched_tf[i]
-            m_pointers[i] += 1
+            if m_pointers[i] + 1 > n + 1 || m_prefs[m_pointers[i] + 1, i] == 0
+                m_matched_tf[i] = true
+            end
         end
     end
 end
