aca7717fc94dbbcad9dcb174fa41089cdc30a876
nswa17 <nswa17@gmail.com>
2016-06-17 20:10:33 +0900

    Slightly efficient

--- da.jl
+++ da.jl
@@ -31,7 +31,7 @@ function call_match(m_prefs, f_prefs, rec=false, m_first=true)
     m_offers = zeros(T, 2, m+1)
     m_offers[1, 1] = 1
 
-    f_pointers = rec ? recursive_da_match(m, n, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers) : da_match(m, n, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers)
+    rec ? recursive_da_match(m, n, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers) : da_match(m, n, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers)
     return m_first ? convert_pointer_to_list(m, m_pointers, f_pointers, f_prefs) : reverse(convert_pointer_to_list(m, m_pointers, f_pointers, f_prefs))
 end
 
@@ -55,6 +55,7 @@ function convert_pointer_to_list(m::Int, m_pointers, f_pointers, f_prefs)
     return m_matched, f_matched
 end
 
+"""
 function proceed_pointer!(m::Int, n::Int, m_pointers, m_matched_tf, m_prefs)
     for i in 1:m
         if m_pointers[i] + 1 > n + 1
@@ -67,16 +68,33 @@ function proceed_pointer!(m::Int, n::Int, m_pointers, m_matched_tf, m_prefs)
         end
     end
 end
+"""
+function proceed_pointer!(m::Int, n::Int, m_pointers, m_matched_tf, m_prefs)
+    for i in 1:m
+        if m_pointers[i] > n
+            m_matched_tf[i] = true
+        else
+            if !m_matched_tf[i]
+                m_pointers[i] += 1
+                if m_prefs[m_pointers[i], i] == 0
+                    m_matched_tf[i] = true
+                end
+            end
+        end
+    end
+end
 
 function create_offers!(m, m_prefs, m_matched_tf, m_pointers, m_offers)
     c::Int = 1
     for i in 1:m
         if !m_matched_tf[i] && m_prefs[m_pointers[i], i] != 0
-            m_offers[:, c] = [i, m_prefs[m_pointers[i], i]]
+            m_offers[1, c] = i
+            m_offers[2, c] = m_prefs[m_pointers[i], i]
             c += 1
         end
     end
-    m_offers[:, c] = [0, 0]
+    m_offers[1, c] = 0
+    m_offers[2, c] = 0
 end
 
 function decide_to_accept!(f_pointers, f_ranks, f_prefs, m_offers, m_matched_tf)
@@ -87,7 +105,7 @@ function decide_to_accept!(f_pointers, f_ranks, f_prefs, m_offers, m_matched_tf)
                 m_matched_tf[f_prefs[f_pointers[m_offers[2, k]], m_offers[2, k]]] = false
             end
             f_pointers[m_offers[2, k]] = f_ranks[m_offers[1, k], m_offers[2, k]]
-            m_matched_tf[m_offers[1, k]] = true###osoi
+            m_matched_tf[m_offers[1, k]] = true
         end
     end
 end
@@ -96,11 +114,8 @@ function recursive_da_match(m::Int, n::Int, f_ranks, m_prefs, f_prefs, m_pointer
     proceed_pointer!(m, n, m_pointers, m_matched_tf, m_prefs)
     create_offers!(m, m_prefs, m_matched_tf, m_pointers, m_offers)
     decide_to_accept!(f_pointers, f_ranks, f_prefs, m_offers, m_matched_tf)
-    if m_offers[1, 1] == 0
-        return f_pointers
-    else
-        recursive_da_match(m::Int, n::Int, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers)
-    end
+    m_offers[1, 1] == 0 && return
+    recursive_da_match(m, n, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers)
 end
 
 function da_match(m::Int, n::Int, f_ranks, m_prefs, f_prefs, m_pointers, f_pointers, m_matched_tf, m_offers)
@@ -109,7 +124,6 @@ function da_match(m::Int, n::Int, f_ranks, m_prefs, f_prefs, m_pointers, f_point
         create_offers!(m, m_prefs, m_matched_tf, m_pointers, m_offers)
         decide_to_accept!(f_pointers, f_ranks, f_prefs, m_offers, m_matched_tf)
     end
-    return f_pointers
 end
 
 function call_simple_match(m_prefs, f_prefs, m_first = true)
@@ -123,10 +137,8 @@ function call_simple_match(m_prefs, f_prefs, m_first = true)
         else
             UInt64
         end
-    m::Int
-    n::Int
-    m = size(m_prefs, 2)
-    n = size(f_prefs, 2)
+    m::Int = size(m_prefs, 2)
+    n::Int = size(f_prefs, 2)
     if !m_first
         m, n = n, m
         m_prefs, f_prefs = f_prefs, m_prefs
@@ -141,7 +153,7 @@ function call_simple_match(m_prefs, f_prefs, m_first = true)
         for i in 1:m
             if !m_matched_tf[i]
                 j = m_prefs[m_pointers[i], i]
-                j == 0 && continue#####################
+                j == 0 && continue
                 if f_matched[j] == 0
                     if f_ranks[end, j] > f_ranks[i, j]
                         f_matched[j] = i
@@ -215,8 +227,8 @@ end
 function check_data(m_prefs, f_prefs)
     m = size(m_prefs, 2)
     n = size(f_prefs, 2)
-    size(m_prefs) != (n+1, m) && error("the size of m_prefs must be (n+1, m)")
-    size(f_prefs) != (m+1, n) && error("the size of f_prefs must be (m+1, n)")
+    size(m_prefs, 1) != n+1 && error("the size of m_prefs must be (n+1, *)")
+    size(f_prefs, 1) != m+1 && error("the size of f_prefs must be (m+1, *)")
     all([Set(m_prefs[:, i]) == Set(0:n) for i in 1:m]) || error("error in m_prefs")
     all([Set(f_prefs[:, j]) == Set(0:m) for j in 1:n]) || error("error in f_prefs")
     return true
